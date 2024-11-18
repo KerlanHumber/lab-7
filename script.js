7d53de5d33a91acfb59759332ca952281f713b8b
Kerlan Augustine <kerlanhumber@gmail.com>
2024-11-17 22:33:57 -0500

    Implemented form event

--- script.js
+++ script.js
@@ -17,3 +17,12 @@ const keyMessage  = document.getElementById('keyMessage');
 keyInput.addEventListener('keydown', (event) => {
     keyMessage.textContent = `Last key pressed: ${event.key}`;
 });
+// Form Events
+const myForm = document.getElementById('myForm');
+const formMessage  = document.getElementById('formMessage');
+
+myForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    formMessage.textContent = 'Form submitted successfully';
+});
+
